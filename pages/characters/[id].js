52a775d44610fa7680f3188f05f9959d766c4042
Veliko <Kardziev>
2023-06-22 15:52:25 +0200

    solving challenge

---
+++ pages/characters/[id].js
@@ -0,0 +1,42 @@
+import Card from "../../components/Card";
+import Layout from "../../components/Layout";
+import useSWR from "swr";
+import { useRouter } from "next/router";
+
+const url = "https://swapi.dev/api";
+const fetcher = async (url) => {
+  const res = await fetch(url);
+
+  // If the status code is not in the range 200-299,
+  // we still try to parse and throw it.
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    // Attach extra info to the error object.
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+
+  return res.json();
+};
+
+export default function Character() {
+  const router = useRouter();
+  const { id } = router.query;
+  const { data, error, isLoading } = useSWR(url + `/people/${id}`, fetcher);
+
+  if (error) return <div>{error.message}</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  return (
+    <Layout>
+      <Card
+        id={id}
+        name={data.name}
+        height={data.height}
+        eyeColor={data.eye_color}
+        birthYear={data.birth_year}
+      />
+    </Layout>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,43 @@
 import styled from "styled-components";
 import Link from "next/link";
 import Layout from "../components/Layout";
+import useSWR from "swr";
+
+const url = "https://swapi.dev/api/";
+const fetcher = async (url) => {
+  const res = await fetch(url);
+
+  // If the status code is not in the range 200-299,
+  // we still try to parse and throw it.
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    // Attach extra info to the error object.
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+
+  return res.json();
+};
 
 export default function HomePage() {
+  const { data, error, isLoading } = useSWR(url + "/people", fetcher);
+  if (error) return <div>{error.message}</div>;
+  if (isLoading) return <div>loading...</div>;
+  const listPeople = data.results;
   return (
     <Layout>
       <h1>React Data Fetching: Star Wars</h1>
       <List>
-        <li>
-          <StyledLink href="/characters/1">Luke Skywalker</StyledLink>
-        </li>
-        <li>
-          <StyledLink href="/characters/2">C-3PO</StyledLink>
-        </li>
-        <li>
-          <StyledLink href="/characters/3">R2-D2</StyledLink>
-        </li>
-        <li>
-          <StyledLink href="/characters/4">Darth Vader</StyledLink>
-        </li>
+        {listPeople.map((person, index) => {
+          return (
+            <li key={index}>
+              <StyledLink href={"/characters/" + (index + 1)}>
+                {person.name}
+              </StyledLink>
+            </li>
+          );
+        })}
       </List>
     </Layout>
   );
